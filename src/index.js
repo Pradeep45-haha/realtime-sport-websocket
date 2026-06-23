51b74b191ffc2aa4c38708d714d8ed1d830003cf
Pradeep Sharma <ironpradeep991@gmail.com>
2026-06-23 14:08:41 +0530

    db connect removed from index

--- src/index.js
+++ src/index.js
@@ -10,27 +10,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-import { Pool } from "pg";
-
-const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  database: "rt_sport_db",
-  port: 5432,
-  password: "AkjjHeso123@#",
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-  maxLifetimeSeconds: 60,
-});
-
-pool.connect();
-
-pool.on("connect", (client) => {
-  console.log("client connected to DB");
-});
-
-pool.on("error", (err) => {
-  console.log(`error while connecting to DB ${err.message}`);
-});
